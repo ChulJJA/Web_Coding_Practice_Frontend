d68165673f7f725c7b642aeaca614891893c5d80
CHUL SEUNG LEE <fpsemfaos@gmail.com>
2024-10-01 22:21:52 -0400

    Control Home opacity

--- Portfolio/src/main.js
+++ Portfolio/src/main.js
@@ -8,3 +8,9 @@ document.addEventListener('scroll', () => {
         header.classList.remove('header--dark');
     }
 });
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
